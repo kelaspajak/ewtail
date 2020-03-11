8e47434a6d242c2597ba09ecc92d28d531112dd1
berlamhenderson <less.summerize@gmail.com>
2020-03-11 15:30:22 +0800

    initial commit

---
+++ _includes/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+  },
+  variants: {
+    borderWidth: ['responsive', 'first', 'hover', 'focus'],
+    boxShadow: ['hover'],
+    margin: ['first'],
+  },
+  corePlugins: {
+    float: false,
+    objectFit: false,
+    objectPosition: false,
+  },
+  plugins: [],
+}
